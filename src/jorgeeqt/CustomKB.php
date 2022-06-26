bced9e112c8624054bda7b6e04b7bffa60c2bcf7
Jorgeeqt <104584243+Jorgeeqt@users.noreply.github.com>
2022-06-26 14:04:48 -0500

    Create CustomKB.php

---
+++ src/jorgeeqt/CustomKB.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace jorgeeqt;
+
+use jorgeeqt/KBListener;
+use jorgeeqt/command/KBCommand;
+use pocketmine/Server;
+use pocketmine/plugin/PluginBase;
+use pocketmine/utils/{TextFormat, Config};
+
+class CustomKB extends PluginBase {
+  
+  private static CustomKB $instance;
+  
+  public static function getInstance() CustomKB
+  {
+    return self::$instance;
+  }
+  
+  public function onEnable(): void
+  {
+    self::$instance = $this;
+    $this->getLogger()->notice(
+      "Custom Knockback
+       Enabled Sussefully"
+    );
+    $this->getServer()->getCommandMap()->register("kb", new KBCommand($this));
+    $this->getServer()->getPluginManager()->registerEvents(new KBListener($this), $this);
+    $this->saveResource("config.yml");
+  }
+}   
